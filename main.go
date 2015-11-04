4f42c20e4eac8a2c7528e715f5d5b66de16925e3
MP Droog <mpdroog@icloud.com>
2015-11-04 15:18:06 +0100

    Own radius implementation, first iteration

--- main.go
+++ main.go
@@ -2,62 +2,207 @@ package main
 
 import (
     "fmt"
-    "github.com/bronze1man/radius"
+    "net"
+
+    "io"
+    "encoding/binary"
+    //"crypto"
+    "crypto/hmac"
+    "crypto/md5"
+    "bytes"
 )
+const AUTH_PORT = 1812
+const ACCOUNTING_PORT = 1813
 
-type radiusService struct{}
+type Attr struct {
+    Type uint8
+    Length uint8
+    Value []byte
+}
 
-func (p radiusService) RadiusHandle(request *radius.Packet) *radius.Packet {
-    // a pretty print of the request.
-    fmt.Printf("[Authenticate] %s\n", request.String())
-    npac := request.Reply()
-    switch request.Code {
-    case radius.AccessRequest:
-        // check username and password
-        if request.GetUsername() == "a" && request.GetPassword() == "a" {
-            npac.Code = radius.AccessAccept
-            return npac
-        } else {
-            npac.Code = radius.AccessReject
-            npac.AVPs = append(npac.AVPs, radius.AVP{Type: radius.ReplyMessage, Value: []byte("you dick!")})
-            return npac
+type Packet struct {
+    Code uint8
+    Identifier uint8
+    Len uint16
+    Auth []byte // Request Authenticator
+    Attrs map[AttributeType]Attr
+}
+
+func Decode(buf []byte, n int) (*Packet, error) {
+    p := &Packet{}
+    p.Code = buf[0]
+    p.Identifier = buf[1]
+    p.Len = uint16(buf[2] + buf[3])
+    p.Auth = buf[4:20] // 16 octets
+    p.Attrs = make(map[AttributeType]Attr)
+
+    // attrs
+    i := 20
+    for {
+        if i >= n {
+            break
         }
-    case radius.AccountingRequest:
-        // accounting start or end
-        npac.Code = radius.AccountingResponse
-        return npac
-    default:
-        npac.Code = radius.AccessAccept
-        return npac
+
+        attr := Attr{
+            Type: buf[i],
+            Length: buf[i+1],
+        }
+        b := i+2
+        e := b + int(attr.Length) -2 // Length is including type+Length fields
+        attr.Value = buf[b:e]
+        p.Attrs[ AttributeType(attr.Type) ] = attr
+
+        i = e
     }
+    return p, nil
+}
+
+// Encode msg for validation
+func Encode(p *Packet) []byte {
+    b := make([]byte, 1024)
+    b[0] = p.Code
+    b[1] = p.Identifier
+    // Skip Len for now 2+3
+    copy(b[4:20], p.Auth)
+    written := 20
+
+    bb := b[20:]
+    for _, attr := range p.Attrs {
+        aLen := len(attr.Value) +2 // add type+len fields
+        if aLen > 255 || aLen < 2 {
+            panic("Value too big for attr")
+        }
+        bb[0] = attr.Type
+        bb[1] = uint8(aLen)
+        copy(bb[2:], attr.Value)
+
+        written += aLen
+        bb = bb[aLen:]
+    }
+
+    // Now set Len
+    binary.BigEndian.PutUint16(b[2:4], uint16(written))
+    fmt.Println("Enc", b[:written])
+    return b[:written]
+}
+
+func Validate(p *Packet) bool {
+    if check, ok := p.Attrs[MessageAuthenticator]; ok {
+        fmt.Println("MessageAuthenticator found!")
+        h := md5.New()
+        temp := Encode(p)
+        //h.Write(temp[0:4])
+        //h.Write([]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+        //h.Write(temp[20:])
+        h.Write(temp)
+        io.WriteString(h, "secret")
+        
+        if !hmac.Equal(check.Value, h.Sum(nil)) {
+            return false
+        }
+    }
+    return true
 }
 
 func main() {
-    s := radius.NewServer(":1812", "secret", radiusService{})
+    // auth
+    addr, e := net.ResolveUDPAddr("udp", "127.0.0.1:1813")
+    if e != nil {
+        panic(e)
+    }
+    server, e := net.ListenUDP("udp", addr)
+    if e != nil {
+        panic(e)
+    }
+    defer server.Close()
 
-    // or you can convert it to a server that accept request 
-    // from some host with different secret
-    // cls := radius.NewClientList([]radius.Client{
-    //      radius.NewClient("127.0.0.1", "secret1"),
-    //      radius.NewClient("10.10.10.10", "secret2"),
-    // })
-    // s.WithClientList(cls)
+    // Start reading packets
+    buf := make([]byte, 1024)
+    n, client, e := server.ReadFromUDP(buf)
+    if e != nil {
+        panic(e)
+    }
+    fmt.Println("RAW", buf)
 
-    signalChan := make(chan os.Signal, 1)
-    signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-    errChan := make(chan error)
-    go func() {
-        fmt.Println("waiting for packets...")
-        err := s.ListenAndServe()
-        if err != nil {
-            errChan <- err
+    p, e := Decode(buf, n)
+    if e != nil {
+        panic(e)
+    }
+    fmt.Println(fmt.Sprintf("%+v", p))
+    if !Validate(p) {
+        panic("Validate failed!")
+    }
+
+    if PacketCode(p.Code) == AccessRequest {
+        // Auth
+        user := ""
+        password := ""
+        for _, attr := range p.Attrs {
+            if AttributeType(attr.Type) == UserName {
+                user = string(attr.Value)
+            }
+            if AttributeType(attr.Type) == UserPassword {
+                h := md5.New()
+                h.Write([]byte("secret"))
+                h.Write(p.Auth)
+                digest := h.Sum(nil)
+                pass := attr.Value
+                if len(pass) != 16 {
+                    panic("Password field wrong?")
+                }
+                for i := 0; i < len(pass); i++ {
+                    pass[i] = pass[i] ^ digest[i]
+                }
+                pass = bytes.TrimRight(pass, string([]rune{0}))
+                password = string(pass)
+            }
         }
-    }()
-    select {
-    case <-signalChan:
-        log.Println("stopping server...")
-        s.Stop()
-    case err := <-errChan:
-        log.Println("[ERR] %v", err.Error())
+        fmt.Println(user, password)
+
+        // Packet
+        np := &Packet{
+            Code: uint8(AccessReject),
+            Identifier: p.Identifier,
+            Auth: p.Auth, // Set req auth
+            Len: 0, // Set by Encode
+            Attrs: make(map[AttributeType]Attr),
+        }
+        msg := Attr{
+            Type: uint8(ReplyMessage),
+            Value: []byte("Invalid user or pass"),
+        }
+        msg.Length = uint8(2 + len(msg.Value))
+        np.Attrs[ReplyMessage] = msg
+
+        if user == "herp" && password == "derp" {
+            fmt.Println("Valid user+pass")
+            np.Code = uint8(AccessAccept)
+            msg := Attr{
+                Type: uint8(ReplyMessage),
+                Value: []byte("Accepted."),
+            }
+            msg.Length = uint8(2 + len(msg.Value))
+            np.Attrs[ReplyMessage] = msg
+        }
+
+        // Encode
+        nb := Encode(np)
+        //binary.BigEndian.PutUint16(nb[2:4], uint16(len(nb)) ) //uint16(4 + 16 + len(nb[20:])))
+
+        // Set right Response Authenticator
+        //np.Auth = MD5(Code+ID+Length+RequestAuth+Attributes+Secret)
+        h := md5.New()
+        h.Write(nb)
+        h.Write([]byte("secret"))
+        res := h.Sum(nil)[:16]
+        copy(nb[4:20], res)
+
+        fmt.Println("OUT", nb)
+        // Reply!
+        if _, e := server.WriteTo(nb, client); e != nil {
+            panic(e)
+        }
+
+        fmt.Println("Done!")
     }
 }
---
+++ test/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+set -u
+set -x
+# Usage: radclient [options] server[:port] <command> [<secret>]
+#  <command>    One of auth, acct, status, coa, or disconnect.
+#  -c count    Send each packet 'count' times.
+#  -d raddb    Set dictionary directory.
+#  -f file     Read packets from file, not stdin.
+#  -F          Print the file name, packet number and reply code.
+#  -h          Print usage help information.
+#  -i id       Set request id to 'id'.  Values may be 0..255
+#  -n num      Send N requests/s
+#  -p num      Send 'num' packets from a file in parallel.
+#  -q          Do not print anything out.
+#  -r retries  If timeout, retry sending the packet 'retries' times.
+#  -s          Print out summary information of auth results.
+#  -S file     read secret from file, not command line.
+#  -t timeout  Wait 'timeout' seconds before retrying (may be a floating point number).
+#  -v          Show program version information.
+#  -x          Debugging mode.
+#  -4          Use IPv4 address of server
+#  -6          Use IPv6 address of server.
+cat auth.txt | radclient 127.0.0.1:1813 auto secret -x
+#radclient 127.0.0.1:1813 auto secret -f ./acct-start.txt -4 -x
+#radclient 127.0.0.1 auto secret -f acct-update.txt -x
+#radclient 127.0.0.1 auto secret -f acct-stop.txt -x
