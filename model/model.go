45f544753280b07dc899416b4308786975266356
MP Droog <mpdroog@icloud.com>
2015-11-05 17:30:41 +0100

    Update queries for accounting

--- model/model.go
+++ model/model.go
@@ -1,44 +1,98 @@
 package model;
 
-import "database/sql"
-import _ "github.com/go-sql-driver/mysql"
-
-var db *sql.DB
+import (
+	"radiusd/config"
+	"time"
+	"fmt"
+)
 
 type User struct {
-	ActiveUntil // Account active until YYYY-MM-DD
-	BlockRemain // Remaining bandwidth
+	ActiveUntil *string // Account active until YYYY-MM-DD
+	BlockRemain *int64 // Remaining bandwidth
+	Ok bool
 }
 
-func Init(driver string, dsn string) error {
-	var e error
-	db, e = sql.Open(driver, dsn)
+func Auth(user string, pass string) (User, error) {
+	u := User{}
+	e := config.DB.QueryRow(
+		`SELECT
+			block_remaining,
+			active_until,
+			1
+		FROM
+			user			
+		WHERE
+			user = ?
+		AND
+			pass = ?`,
+		user, pass,
+	).Scan(&u.BlockRemain, &u.ActiveUntil, &u.Ok)
+	if e == config.ErrNoRows {
+		return u, nil
+	}
+	return u, e
+}
+
+func SessionAdd(sessionId, user string, nasIp string, hostname string) error {
+	res, e := config.DB.Exec(
+		`INSERT INTO
+			session
+		(session_id, user, time_added, nas_ip, hostname)
+		VALUES
+		(?, ?, ?, ?, ?)`,
+		sessionId, user, time.Now().Unix(), nasIp, hostname,
+	)
 	if e != nil {
 		return e
 	}
-	if e := db.Ping(); e != nil {
+	affect, e := res.RowsAffected()
+	if e != nil {
 		return e
+	}
+	if affect != 1 {
+		return fmt.Errorf(
+			"Affect fail for sess=%s user=%s",
+			sessionId, user,
+		)
 	}
 	return nil
 }
 
-func Close() error {
-	return db.Close()
+func SessionRemove(sessionId string) error {
+	res, e := config.DB.Exec(
+		`DELETE FROM
+			session
+		WHERE
+			session_id = ?`,
+		sessionId,
+	)
+	if e != nil {
+		return e
+	}
+	affect, e := res.RowsAffected()
+	if e != nil {
+		return e
+	}
+	if affect != 1 {
+		return fmt.Errorf(
+			"Affect fail for sess=%s",
+			sessionId,
+		)
+	}
+	return nil
 }
 
-func Auth(user string, pass string) (User, error) {
-	u := &User{}
-	e := db.QueryRow(
-		`SELECT
-			block_remaining,
-			active_until
-		FROM
-			user			
+// Delete all sessions for this app
+func SessionClear(hostname string) (int64, error) {
+	res, e := config.DB.Exec(
+		`DELETE FROM
+			session
 		WHERE
-			radius_login = ?
-		AND
-			radius_pass = ?`,
-		user, pass,
-	).Scan(&u.BlockRemain, &u.ActiveUntil)
-	return u, e
+			hostname = ?`,
+		hostname,
+	)
+	if e != nil {
+		return 0, e
+	}
+	return res.RowsAffected()
 }
