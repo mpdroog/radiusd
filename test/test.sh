88caea23f3e3871940987eb7a4416f7ddef9afed
MP Droog <mpdroog@icloud.com>
2015-12-23 17:57:21 +0100

    Test. MS-CHAPv2

--- test/test.sh
+++ test/test.sh
@@ -13,3 +13,4 @@ cat auth.txt | radclient 127.0.0.1 auto secret -x # 0 conns remain
 
 cat auth-chap.txt | radclient 127.0.0.1 auto secret -x -i 217 # CHAP-auth (md5)
 cat auth-mschapv1.txt | radclient -x 127.0.0.1 auto secret # MS-CHAPv1
+cat auth-mschapv2.txt | radclient -x 127.0.0.1 auto secret # MS-CHAPv2
