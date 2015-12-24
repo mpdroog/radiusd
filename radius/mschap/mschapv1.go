d4364bd15ddc4da4474e59c3e99ddc77092657a0
MP Droog <mpdroog@icloud.com>
2015-12-24 17:10:38 +0100

    Bugfix. hashHash expected for MPPE not challengeresponse

--- radius/mschap/mschapv1.go
+++ radius/mschap/mschapv1.go
@@ -95,15 +95,7 @@ func ntChallengeResponse(challenge []byte, passHash []byte) ([]byte, error) {
 // supplied value.
 // OUT2 = MPPE is to support encryption
 func Encryptv1(challenge []byte, pass string) ([]byte, []byte, error) {
-	/*
-	 *	According to RFC 2548 we
-	 *	should send NT hash.  But in
-	 *	practice it doesn't work.
-	 *	Instead, we should send nthashhash
-	 *	This is an error in RFC 2548.
-	 * https://github.com/FreeRADIUS/freeradius-server/blob/5ea87f156381174ea24340db9b450d4eca8189c9/src/modules/rlm_mschap/rlm_mschap.c#L1956
-	 */
-	passHash := hashNtPasswordHash(ntPasswordHash(ntPassword(pass)))
+	passHash := ntPasswordHash(ntPassword(pass))
 	res, e := ntChallengeResponse(challenge, passHash)
 	if e != nil {
 		return nil, nil, e
@@ -116,7 +108,15 @@ func Encryptv1(challenge []byte, pass string) ([]byte, []byte, error) {
 			return nil, nil, e
 		}
 		mppe = append(mppe, lm...)
-		mppe = append(mppe, passHash...)
+		/*
+		 *	According to RFC 2548 we
+		 *	should send NT hash.  But in
+		 *	practice it doesn't work.
+		 *	Instead, we should send nthashhash
+		 *	This is an error in RFC 2548.
+		 * https://github.com/FreeRADIUS/freeradius-server/blob/5ea87f156381174ea24340db9b450d4eca8189c9/src/modules/rlm_mschap/rlm_mschap.c#L1956
+		 */
+		mppe = append(mppe, hashNtPasswordHash(passHash)...)
 		mppe = append(mppe, []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}...)
 	}
 
