c0270aaaabd13c84385673a680f4fee2701ce6b2
mark <mpdroog@rootdev.nl>
2015-12-28 16:22:53 +0100

    Bugfix. Print key that was not available

--- radius/Packet.go
+++ radius/Packet.go
@@ -14,6 +14,7 @@ import (
 	"crypto/md5"
 	"encoding/binary"
 	"radiusd/config"
+	"fmt"
 )
 
 type Attr struct {
@@ -48,7 +49,7 @@ func (p *Packet) Attr(key AttributeType) []byte {
 			return a.Value
 		}
 	}
-	panic("No such key=")
+	panic(fmt.Sprintf("No such key %+v", k))
 }
 // If requested attribute exists
 func (p *Packet) HasAttr(key AttributeType) bool {
