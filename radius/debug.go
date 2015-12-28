45635819abd822bbce01a9b7e49a565876985395
mark <mpdroog@rootdev.nl>
2015-12-28 18:11:23 +0100

    Logic. Invert everything to string to everything to byte

--- radius/debug.go
+++ radius/debug.go
@@ -4,23 +4,15 @@ import (
 	"fmt"
 )
 
-func asByte(a AttributeType) bool {
+func asString(a AttributeType) bool {
 	bytes := map[AttributeType]bool {
-		UserPassword: true,
-		NASIPAddress: true,
-		NASPort: true,
-		NASPortType: true,
-		AcctStatusType: true,
-		AcctAuthentic: true,
-		FramedIPAddress: true,
-		AcctSessionTime: true,
-		AcctInputPackets: true,
-		AcctOutputPackets: true,
-		AcctInputOctets: true,
-		AcctOutputOctets: true,
-		AcctTerminateCause: true,
-		CHAPPassword: true,
-		VendorSpecific: true,
+		UserName: true,
+		CallingStationId: true,
+		CalledStationId: true,
+		NASIdentifier: true,
+		ReplyMessage: true,
+		AcctSessionId: true,
+		ConnectInfo: true,
 	}
 	return bytes[a]
 }
@@ -39,10 +31,10 @@ func debug(p *Packet) string {
 	s := fmt.Sprintf("Code=%s Ident=%d\n", p.Code, p.Identifier)
 	for _, attr := range p.Attrs {
 		b := attr.Bytes()
-		if asByte(attr.Type()) {
-			s += fmt.Sprintf("\t%s = %+v\n", attr.Type(), b)
-		} else {
+		if asString(attr.Type()) {
 			s += fmt.Sprintf("\t%s = %s\n", attr.Type(), string(b))
+		} else {
+			s += fmt.Sprintf("\t%s = %+v\n", attr.Type(), b)
 		}
 	}
 	return s + "\n"
