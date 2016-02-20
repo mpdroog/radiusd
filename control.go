44f1b3e301d6402e748411dcf07981e2d2bae869
MP Droog <mpdroog@icloud.com>
2016-02-20 16:54:09 +0100

    Bugfix. Invalid JSON, Bugfix. Multi JSON response possible on error

--- control.go
+++ control.go
@@ -53,8 +53,8 @@ func doc(w http.ResponseWriter, r *http.Request) {
 // Finish pending jobs and close application
 func shutdown(w http.ResponseWriter, r *http.Request) {
 	if config.Stopping {
-		if _, e := w.Write([]byte(fmt.Sprintf(`{success: true, msg: "Already stopping."}`))); e != nil {
-			httpd.Error(w, e, "Flush failed")
+		if _, e := w.Write([]byte(fmt.Sprintf(`{"success": true, "msg": "Already stopping."}`))); e != nil {
+			config.Log.Printf("control: " + e.Error())
 			return
 		}
 	}
@@ -63,15 +63,21 @@ func shutdown(w http.ResponseWriter, r *http.Request) {
 	config.Stopping = true
 
 	if e := ln.Close(); e != nil {
-		httpd.Error(w, e, `{success: false, msg: "Error stopping HTTP-listener"}`)
+		if _, e := w.Write([]byte(fmt.Sprintf(`{"success": false, "msg": "Error stopping HTTP-listener"}`))); e != nil {
+			config.Log.Printf("control: " + e.Error())
+			return
+		}
 	}
 	for _, sock := range config.Sock {
 		if e := sock.Close(); e != nil {
-			httpd.Error(w, e, `{success: false, msg: "Error stopping listener"}`)
+			if _, e := w.Write([]byte(fmt.Sprintf(`{"success": false, "msg": "Error stopping listener"}`))); e != nil {
+				config.Log.Printf("control: " + e.Error())
+				return
+			}
 		}
 	}
-	if _, e := w.Write([]byte(`{success: true, msg: "Stopped listening, waiting for empty queue."}`)); e != nil {
-		httpd.Error(w, e, "Flush failed")
+	if _, e := w.Write([]byte(`{"success": true, "msg": "Stopped listening, waiting for empty queue."}`)); e != nil {
+		config.Log.Printf("control: " + e.Error())
 		return
 	}
 }
