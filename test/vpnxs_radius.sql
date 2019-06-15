10e1c5b76ec8960d9a1c4ff4b60d4497c72bb191
MP Droog <mpdroog@icloud.com>
2019-06-15 15:08:27 +0200

    DB. Save simple DB-struct to run tests

---
+++ test/vpnxs_radius.sql
@@ -0,0 +1,162 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Local
+ Source Server Type    : MySQL
+ Source Server Version : 100314
+ Source Host           : localhost:3306
+ Source Schema         : vpnxs_radius
+
+ Target Server Type    : MySQL
+ Target Server Version : 100314
+ File Encoding         : 65001
+
+ Date: 15/06/2019 15:06:28
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for accounting
+-- ----------------------------
+DROP TABLE IF EXISTS `accounting`;
+CREATE TABLE `accounting` (
+  `user` varchar(100) NOT NULL,
+  `date` varchar(16) NOT NULL DEFAULT '' COMMENT '1min consolidated YYYY-MM-DD HH:MM',
+  `hostname` varchar(50) NOT NULL COMMENT 'RadiusD-server for unique key',
+  `bytes_in` bigint(15) unsigned NOT NULL COMMENT 'Octet in',
+  `bytes_out` bigint(15) unsigned NOT NULL COMMENT 'Octet out',
+  `packets_in` int(10) unsigned NOT NULL,
+  `packets_out` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`user`,`date`,`hostname`),
+  CONSTRAINT `fk_accounting_user` FOREIGN KEY (`user`) REFERENCES `user` (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for dedi_ip
+-- ----------------------------
+DROP TABLE IF EXISTS `dedi_ip`;
+CREATE TABLE `dedi_ip` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `ip` varchar(50) NOT NULL,
+  `time_added` int(10) unsigned NOT NULL,
+  `time_reserved` int(10) unsigned DEFAULT NULL,
+  `time_updated` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_ip` (`ip`),
+  UNIQUE KEY `unique_user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for dns
+-- ----------------------------
+DROP TABLE IF EXISTS `dns`;
+CREATE TABLE `dns` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  `one` varchar(50) NOT NULL,
+  `two` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_name` (`name`),
+  UNIQUE KEY `unique_dns` (`one`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for product
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `product` varchar(50) NOT NULL,
+  `simultaneous_use` int(10) unsigned NOT NULL COMMENT 'Max sessions',
+  `ratelimit_up` int(10) unsigned DEFAULT NULL,
+  `ratelimit_down` int(10) unsigned DEFAULT NULL,
+  `ratelimit_unit` enum('k','M') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_product` (`product`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of product
+-- ----------------------------
+BEGIN;
+INSERT INTO `product` VALUES (1, 'derp', 1, NULL, NULL, NULL);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for session
+-- ----------------------------
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE `session` (
+  `session_id` varchar(20) NOT NULL,
+  `user` varchar(100) NOT NULL,
+  `nas_ip` varchar(50) NOT NULL COMMENT 'VPN Server',
+  `bytes_in` bigint(10) unsigned NOT NULL,
+  `bytes_out` bigint(10) unsigned NOT NULL,
+  `packets_in` bigint(10) unsigned NOT NULL,
+  `packets_out` bigint(10) unsigned NOT NULL,
+  `session_time` bigint(10) unsigned NOT NULL COMMENT 'Session open in sec',
+  `client_ip` varchar(50) NOT NULL,
+  `assigned_ip` varchar(50) NOT NULL,
+  `time_added` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`session_id`,`user`,`nas_ip`),
+  KEY `fk_session_user` (`user`),
+  CONSTRAINT `fk_session_user` FOREIGN KEY (`user`) REFERENCES `user` (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Active connections.';
+
+-- ----------------------------
+-- Table structure for session_log
+-- ----------------------------
+DROP TABLE IF EXISTS `session_log`;
+CREATE TABLE `session_log` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `bytes_in` bigint(10) unsigned NOT NULL,
+  `bytes_out` bigint(10) unsigned NOT NULL,
+  `packets_in` bigint(10) unsigned NOT NULL,
+  `packets_out` bigint(10) unsigned NOT NULL,
+  `session_id` varchar(20) NOT NULL,
+  `session_time` bigint(10) unsigned NOT NULL COMMENT 'Session open in sec',
+  `user` varchar(100) NOT NULL,
+  `nas_ip` varchar(50) NOT NULL,
+  `client_ip` varchar(50) NOT NULL,
+  `assigned_ip` varchar(50) NOT NULL,
+  `time_added` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_session_log_user` (`user`),
+  CONSTRAINT `fk_session_log_user` FOREIGN KEY (`user`) REFERENCES `user` (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Closed connections.';
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user` varchar(100) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `block_remaining` bigint(20) unsigned DEFAULT NULL,
+  `active_until` date DEFAULT NULL COMMENT 'Account becomes inactive on given date',
+  `dedicated_ip` varchar(50) DEFAULT NULL COMMENT 'Static IP',
+  `product_id` int(10) unsigned NOT NULL,
+  `dns_id` int(3) unsigned DEFAULT NULL COMMENT 'DNS Pri+Sec',
+  `time_added` int(10) unsigned NOT NULL,
+  `time_updated` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_login` (`user`),
+  UNIQUE KEY `unique_ip` (`dedicated_ip`),
+  KEY `fk_user_product` (`product_id`),
+  KEY `fk_user_dns_1` (`dns_id`),
+  CONSTRAINT `fk_user_dns_1` FOREIGN KEY (`dns_id`) REFERENCES `dns` (`id`),
+  CONSTRAINT `fk_user_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` VALUES (5, 'vpn293b50c891377b94c904b42396e45fd99d', 'derpderp', 0, NULL, NULL, 1, NULL, 2017, NULL);
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
